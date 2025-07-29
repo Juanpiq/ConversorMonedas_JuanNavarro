8048a5da92fc061301fb46c8861237a01250db7b
Juanpiq <juanavarroquintero@gmail.com>
2025-07-28 21:45:11 -0500

    importar gson

--- src/main/java/org/challenge/Main.java
+++ src/main/java/org/challenge/Main.java
@@ -2,6 +2,7 @@ package org.challenge;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(persona);*/
     }
 }
