2a2037388006f1530c9c3b7ca9dc3de7400c5fbc
Juanpiq <juanavarroquintero@gmail.com>
2025-07-31 23:40:48 -0500

    switch de monedas

--- src/main/java/org/challenge/Main.java
+++ src/main/java/org/challenge/Main.java
@@ -10,13 +10,14 @@ public class Main {
         String json = gson.toJson(persona);*/
         Scanner in = new Scanner(System.in);
         int op = 1;
+        String url;
 
         while(op != 7){
             try{
                 System.out.println("""
                                       Sea Bienvenido/a al Conversor de Monedas
                                       1) Dólar =>> Peso Argentino
-                                      2) Peso Argentino ==> Dólar
+                                      2) Peso Argentino =>> Dólar
                                       3) Dólar =>> Real brasileño
                                       4) Real brasileño =>> Dólar
                                       5) Dólar =>> Peso Colombiano
@@ -27,21 +28,63 @@ public class Main {
                 op = in.nextInt();
                 in.nextLine();
                 if(op >= 1 && op <= 7){
-                    } else System.out.println("Opción no disponible, vuelva a intentarlo\n");
+                    if(op == 7)
+                        System.out.println("¡Hasta luego!");
+                    else{
+                        url = EscogerOpcion(in, op);
+                    }
+                } else {
+                    System.out.println("Opción no disponible, vuelva a intentarlo\n");
+                    op = 1;
+                    in.nextLine();
+                }
             }catch(InputMismatchException e){
                 System.out.println("Ingrese un numero válido");
                 in.nextLine();
                 op = 1;
-                continue;
             }finally{
                 System.out.println("\n");
             }
         }
     }
 
-    static void EscogerOpcion(Scanner in, int op){
-        String url1 = "GET https://v6.exchangerate-api.com/v6/"+API_KEY+"/latest/";
+    static String EscogerOpcion(Scanner in, int op){
+        String moneda1 = null, moneda2 = null;
 
+        switch(op){
+            case 1:{
+                moneda1 = "USD";
+                moneda2 = "ARS";
+                break;
+            }
+            case 2:{
+                moneda1 = "ARS";
+                moneda2 = "USD";
+                break;
+            }
+            case 3:{
+                moneda1 = "USD";
+                moneda2 = "BRL";
+                break;
+            }
+            case 4:{
+                moneda1 = "BRL";
+                moneda2 = "USD";
+                break;
+            }
+            case 5:{
+                moneda1 = "USD";
+                moneda2 = "COP";
+                break;
+            }
+            case 6:{
+                moneda1 = "COP";
+                moneda2 = "USD";
+                break;
+            }
+            default: break;
         }
+        return "https://v6.exchangerate-api.com/v6/"+API_KEY+"/pair/"+moneda1+"/"+moneda2;
+    }
 
 }
